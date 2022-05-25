9f8cce3ffccecda87acab029bfe10707ce24f7ee
tarieltairov <tarieltairov1@gmail.com>
2022-05-26 01:20:44 +0600

    create footer

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Header from "../src/components/Header/Header";
 import MainSlider from "../src/components/MainSlider/MainSlider";
+import Footer from "../src/components/Footer/Footer";
 
 export default function Home() {
   return (
     <>
       <Header />
       <MainSlider />
+      <Footer />
     </>
   );
 }
