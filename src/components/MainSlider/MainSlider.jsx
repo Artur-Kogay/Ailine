e4e43b584bb655fbf425a90baa91ac0263546b89
Daniel <donisuper04@gmail.com>
2022-05-26 17:25:20 +0600

    added header and slider block

--- src/components/MainSlider/MainSlider.jsx
+++ src/components/MainSlider/MainSlider.jsx
@@ -1,18 +1,44 @@
 import React from "react";
-import Image from "next/image"
+import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
-import style from "./MainSlider.module.scss"
-import PhotoForSlide1 from "../../assets/jpg/mainBackground.jpg"
+import style from "./MainSlider.module.scss";
+import PhotoForSlide1 from "../../assets/jpg/mainBackground.jpg";
 
 const MainSlider = () => {
   return (
     <div className={style.swiperBlock}>
       <Swiper navigation={true} modules={[Navigation]} className={style.swiper}>
-        <SwiperSlide><Image src={PhotoForSlide1} alt={"slide 1"}/></SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide className={style.swiperItem}>
+          <div className={style.background}></div>
+          <div className={"container"}>
+            <div className={style.content}>
+              <h1>Ваш надежный партнер!</h1>
+              <h3>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Adipisci, labore. Lorem ipsum dolor <br /> sit amet, consectetur
+                adipisicing elit. Laborum, rerum.
+              </h3>
+              <a href="#">Contact us</a>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className={style.swiperItem}>
+          <div className={style.background}></div>
+          <div className={"container"}>
+            <div className={style.content}>
+              <h1>Ваш надежный партнер!</h1>
+              <h3>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Adipisci, labore. Lorem ipsum dolor <br /> sit amet, consectetur
+                adipisicing elit. Laborum, rerum.
+              </h3>
+              <a href="#">Contact us</a>
+            </div>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
