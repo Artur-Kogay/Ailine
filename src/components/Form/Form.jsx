b5ee3faaa261347fe3617c89352b763f3c6e086c
Daniel <donisuper04@gmail.com>
2022-05-29 23:43:38 +0600

    updates

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,46 +1,58 @@
-import React from 'react';
-import Image from 'next/image';
-import cl from './Form.module.scss'
-import icon from '../../assets/contact-icon.png'
+import React from "react";
+import Image from "next/image";
+import cl from "./Form.module.scss";
+import icon from "../../assets/contact-icon.png";
+import style from "../Services/Services.module.scss";
 const Form = () => {
-    return (
-        <div className={cl.form}>
-            <div className={cl.largest_inner}>
-                <div className={cl.largest_inner_top}>
-                    <div className={cl.largest_inner_top_left}>
-                        <h2>Our Contact now</h2>
-                        <h1>We Lost Guide  <span>To Immediately For Contact  </span>Now.</h1>
-                    </div>
-                    <div className={cl.largest_inner_top_right}>
-                        <p>Lorem ipsum is simply free text dolor sit amet, consectetur notted adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua lonm andhn.</p>
-                    </div>
-                </div>
-                <div className={cl.inp_blocks}>
-                    <input type="text" placeholder='Name*' />
-                    <input type="text" placeholder='Email*' />
-                </div>
-                <div className={cl.inp_blocks}>
-                    <input type="text" placeholder='Phone*' />
-                    <input type="text" placeholder='Website' />
-                </div>
-                <textarea name="" id="" cols="30" rows="10" placeholder='Comments/Message'></textarea>
-                <button>Submit Now</button>
-            </div>
-            <div className={cl.contact}>
-                <div className={cl.contact_now}>
-                    <div className={cl.contact_now_inner}>
-                        <div className={cl.contact_now_inner_left}>
-                            <Image src={icon} />
-                            <h3>Start Your Best it-Service for Business</h3>
-                        </div>
-                        <div className={cl.contact_now_inner_right}>
-                            <button>Contact Now</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className={cl.form}>
+      <div className={cl.largest_inner}>
+        <div className={cl.header}>
+          <h2>
+            We Business and{" "}
+            <span className={"greenContent"}>It Solutions Of Total</span>
+            -Services
+          </h2>
+          <span className={cl.typography}>
+            <p>
+              Lorem ipsum is simply free text dolor sit amet, consectetur notted
+              adipisicing elit sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua lonm andhn.
+            </p>
+          </span>
         </div>
-    );
+        <div className={cl.inp_blocks}>
+          <input type="text" placeholder="Name*" />
+          <input type="text" placeholder="Email*" />
+        </div>
+        <div className={cl.inp_blocks}>
+          <input type="text" placeholder="Phone*" />
+          <input type="text" placeholder="Website" />
+        </div>
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          placeholder="Comments/Message"
+        ></textarea>
+        <button>Submit Now</button>
+      </div>
+      <div className={cl.contact}>
+        <div className={cl.contact_now}>
+          <div className={cl.contact_now_inner}>
+            <div className={cl.contact_now_inner_left}>
+              <Image src={icon} />
+              <h3>Start Your Best it-Service for Business</h3>
+            </div>
+            <div className={cl.contact_now_inner_right}>
+              <button>Contact Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Form;
--- src/components/LargestBlock/Largest.jsx
+++ src/components/LargestBlock/Largest.jsx
@@ -1,111 +1,152 @@
-import React from 'react';
-import cl from './Largest.module.scss';
+import React from "react";
+import cl from "./Largest.module.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css/pagination";
-import { Autoplay } from 'swiper'
-import Image from 'next/image';
-import q from '../../assets/swip1.jpg';
-import w from '../../assets/swip2.jpg';
-import e from '../../assets/swip3.jpg';
-import r from '../../assets/swip4.jpg';
-import { useMediaQuery } from 'react-responsive';
+import { Autoplay } from "swiper";
+import Image from "next/image";
+import q from "../../assets/swip1.jpg";
+import w from "../../assets/swip2.jpg";
+import e from "../../assets/swip3.jpg";
+import r from "../../assets/swip4.jpg";
+import { useMediaQuery } from "react-responsive";
+import style from "../Services/Services.module.scss";
 const Largest = () => {
-    const isPlanshet = useMediaQuery({ query: '(max-width: 768px)' });
-    const isMobile = useMediaQuery({ query: '(max-width: 425px)' });
-    return (
-        <div className={cl.largest}>
-            <div className={cl.largest_inner}>
-                <h2>Our largest blog</h2>
-                <div className={cl.largest_inner_top}>
-                    <div className={cl.largest_inner_top_left}>
-                        <h1>Apply These <span>Secret Techniques To Improve </span>Blog.</h1>
-                    </div>
-                    <div className={cl.largest_inner_top_right}>
-                        <p>Lorem ipsum is simply free text dolor sit amet, consectetur notted adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua lonm andhn.</p>
-                    </div>
-                </div>
-                <div className={cl.largest_inner_bottom}>
-                    <Swiper
-                        resizeObserver={true}
-                        rewind={true}
-                        slidesPerView={isPlanshet ? 2 : (isMobile ? 1 : 3)}
-                        spaceBetween={40}
-                        className={cl.mySwiper}
-                        autoplay={{
-                            delay: 3500,
-                            disableOnInteraction: false,
-                        }}
-                        modules={[Autoplay]}
-                    >
-                        <SwiperSlide className={cl.swipItem}>
-                            <Image src={q} className={cl.img} />
-                            <div className={cl.card_text_block}>
-                                <h3>By Admin Softnet.<span>19 May 2021</span></h3>
-                                <h2>Where people develop the own potential</h2>
-                                <p>Our offices are located on the traditional, unced. photo booth</p>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide className={cl.swipItem}>
-                            <Image src={w} className={cl.img} />
-                            <div className={cl.card_text_block}>
-                                <h3>By Admin Softnet.<span>19 May 2021</span></h3>
-                                <h2>Where people develop the own potential</h2>
-                                <p>Our offices are located on the traditional, unced. photo booth</p>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide className={cl.swipItem}>
-                            <Image src={e} className={cl.img} />
+  const isPlanshet = useMediaQuery({ query: "(max-width: 768px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 425px)" });
+  return (
+    <div className={cl.largest}>
+      <div className={cl.largest_inner}>
+        <h2>Our largest blog</h2>
+        <div className={cl.header}>
+          <h2>
+            We Business and{" "}
+            <span className={"greenContent"}>It Solutions Of Total</span>
+            -Services
+          </h2>
+          <span className={cl.typography}>
+            <p>
+              Lorem ipsum is simply free text dolor sit amet, consectetur notted
+              adipisicing elit sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua lonm andhn.
+            </p>
+          </span>
+        </div>
+        <div className={cl.largest_inner_bottom}>
+          <Swiper
+            resizeObserver={true}
+            rewind={true}
+            slidesPerView={(isPlanshet && 1) || (3 && isMobile && 2) || 3}
+            spaceBetween={40}
+            className={cl.mySwiper}
+            autoplay={{
+              delay: 3500,
+              disableOnInteraction: false,
+            }}
+            modules={[Autoplay]}
+          >
+            <SwiperSlide className={cl.swipItem}>
+              <Image src={q} className={cl.img} />
+              <div className={cl.card_text_block}>
+                <h3>
+                  By Admin Softnet.<span>19 May 2021</span>
+                </h3>
+                <h2>Where people develop the own potential</h2>
+                <p>
+                  Our offices are located on the traditional, unced. photo booth
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={cl.swipItem}>
+              <Image src={w} className={cl.img} />
+              <div className={cl.card_text_block}>
+                <h3>
+                  By Admin Softnet.<span>19 May 2021</span>
+                </h3>
+                <h2>Where people develop the own potential</h2>
+                <p>
+                  Our offices are located on the traditional, unced. photo booth
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={cl.swipItem}>
+              <Image src={e} className={cl.img} />
 
-                            <div className={cl.card_text_block}>
-                                <h3>By Admin Softnet.<span>19 May 2021</span></h3>
-                                <h2>Where people develop the own potential</h2>
-                                <p>Our offices are located on the traditional, unced. photo booth</p>
-                            </div></SwiperSlide>
-                        <SwiperSlide className={cl.swipItem}>
-                            <Image src={r} className={cl.img} />
-                            <div className={cl.card_text_block}>
-                                <h3>By Admin Softnet.<span>19 May 2021</span></h3>
-                                <h2>Where people develop the own potential</h2>
-                                <p>Our offices are located on the traditional, unced. photo booth</p>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide className={cl.swipItem}>
-                            <Image src={q} className={cl.img} />
-                            <div className={cl.card_text_block}>
-                                <h3>By Admin Softnet.<span>19 May 2021</span></h3>
-                                <h2>Where people develop the own potential</h2>
-                                <p>Our offices are located on the traditional, unced. photo booth</p>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide className={cl.swipItem}>
-                            <Image src={w} className={cl.img} />
-                            <div className={cl.card_text_block}>
-                                <h3>By Admin Softnet.<span>19 May 2021</span></h3>
-                                <h2>Where people develop the own potential</h2>
-                                <p>Our offices are located on the traditional, unced. photo booth</p>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide className={cl.swipItem}>
-                            <Image src={e} className={cl.img} />
+              <div className={cl.card_text_block}>
+                <h3>
+                  By Admin Softnet.<span>19 May 2021</span>
+                </h3>
+                <h2>Where people develop the own potential</h2>
+                <p>
+                  Our offices are located on the traditional, unced. photo booth
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={cl.swipItem}>
+              <Image src={r} className={cl.img} />
+              <div className={cl.card_text_block}>
+                <h3>
+                  By Admin Softnet.<span>19 May 2021</span>
+                </h3>
+                <h2>Where people develop the own potential</h2>
+                <p>
+                  Our offices are located on the traditional, unced. photo booth
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={cl.swipItem}>
+              <Image src={q} className={cl.img} />
+              <div className={cl.card_text_block}>
+                <h3>
+                  By Admin Softnet.<span>19 May 2021</span>
+                </h3>
+                <h2>Where people develop the own potential</h2>
+                <p>
+                  Our offices are located on the traditional, unced. photo booth
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={cl.swipItem}>
+              <Image src={w} className={cl.img} />
+              <div className={cl.card_text_block}>
+                <h3>
+                  By Admin Softnet.<span>19 May 2021</span>
+                </h3>
+                <h2>Where people develop the own potential</h2>
+                <p>
+                  Our offices are located on the traditional, unced. photo booth
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={cl.swipItem}>
+              <Image src={e} className={cl.img} />
 
-                            <div className={cl.card_text_block}>
-                                <h3>By Admin Softnet.<span>19 May 2021</span></h3>
-                                <h2>Where people develop the own potential</h2>
-                                <p>Our offices are located on the traditional, unced. photo booth</p>
-                            </div></SwiperSlide>
-                        <SwiperSlide className={cl.swipItem}>
-                            <Image src={r} className={cl.img} />
-                            <div className={cl.card_text_block}>
-                                <h3>By Admin Softnet.<span>19 May 2021</span></h3>
-                                <h2>Where people develop the own potential</h2>
-                                <p>Our offices are located on the traditional, unced. photo booth</p>
-                            </div>
-                        </SwiperSlide>
-                    </Swiper>
-                </div>
-            </div>
-        </div >
-    );
+              <div className={cl.card_text_block}>
+                <h3>
+                  By Admin Softnet.<span>19 May 2021</span>
+                </h3>
+                <h2>Where people develop the own potential</h2>
+                <p>
+                  Our offices are located on the traditional, unced. photo booth
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={cl.swipItem}>
+              <Image src={r} className={cl.img} />
+              <div className={cl.card_text_block}>
+                <h3>
+                  By Admin Softnet.<span>19 May 2021</span>
+                </h3>
+                <h2>Where people develop the own potential</h2>
+                <p>
+                  Our offices are located on the traditional, unced. photo booth
+                </p>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Largest;
