7efcdb5db035641e8cc569178b8b14e6fe7b367f
tarieltairov <tarieltairov1@gmail.com>
2022-05-31 18:15:48 +0600

    added animate and fix content

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import Image from "next/image";
 import cl from "./Form.module.scss";
 import icon from "../../assets/contact-icon.png";
-import style from "../Services/Services.module.scss";
+import { motion } from 'framer-motion';
 const Form = () => {
   return (
     <div className={cl.form}>
       <div className={cl.largest_inner}>
-        <div className={cl.header}>
+        <motion.div
+          viewport={{ once: true }}
+          initial='hidden'
+          whileInView='visible'
+          transition={{ duration: 0.5 }}
+          variants={{
+            visible: { opacity: 1, scale: 1 },
+            hidden: { opacity: 0, scale: 0 },
+          }}
+          className={cl.header}>
           <h2>
             We Business and{" "}
             <span className={"greenContent"}>It Solutions Of Total</span>
@@ -15,32 +24,75 @@ const Form = () => {
           </h2>
           <span className={cl.typography}>
             <p>
-              Lorem ipsum is simply free text dolor sit amet, consectetur notted
-              adipisicing elit sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua lonm andhn.
+              With long-term cooperation, our company provides its partner with
+              individual partner price for outstaffing and outsourcing services of our enterprises,
+              which will pleasantly surprise you!
             </p>
           </span>
-        </div>
-        <div className={cl.inp_blocks}>
+        </motion.div>
+        <motion.div
+          viewport={{ once: true }}
+          initial='hidden'
+          whileInView='visible'
+          transition={{ duration: 0.5 }}
+          variants={{
+            visible: { opacity: 1, scale: 1 },
+            hidden: { opacity: 0, scale: 0 },
+          }}
+          className={cl.inp_blocks}>
           <input type="text" placeholder="Name*" />
           <input type="text" placeholder="Email*" />
-        </div>
-        <div className={cl.inp_blocks}>
+        </motion.div>
+        <motion.div
+          viewport={{ once: true }}
+          initial='hidden'
+          whileInView='visible'
+          transition={{ duration: 0.5 }}
+          variants={{
+            visible: { opacity: 1, scale: 1 },
+            hidden: { opacity: 0, scale: 0 },
+          }}
+          className={cl.inp_blocks}>
           <input type="text" placeholder="Phone*" />
           <input type="text" placeholder="Website" />
-        </div>
-        <textarea
+        </motion.div>
+        <motion.textarea
+          viewport={{ once: true }}
+          initial='hidden'
+          whileInView='visible'
+          transition={{ duration: 0.5 }}
+          variants={{
+            visible: { opacity: 1, scale: 1 },
+            hidden: { opacity: 0, scale: 0 },
+          }}
           name=""
           id=""
           cols="30"
           rows="10"
           placeholder="Comments/Message"
-        ></textarea>
-        <button>Submit Now</button>
+        ></motion.textarea>
+        <motion.button
+          viewport={{ once: true }}
+          initial='hidden'
+          whileInView='visible'
+          transition={{ duration: 0.5 }}
+          variants={{
+            visible: { opacity: 1, scale: 1 },
+            hidden: { opacity: 0, scale: 0 },
+          }}
+        >Submit Now</motion.button>
       </div>
       <div className={cl.contact}>
         <div className={cl.contact_now}>
-          <div className={cl.contact_now_inner}>
+          <motion.div
+            viewport={{ once: true }}
+            initial='hidden'
+            whileInView='visible'
+            transition={{ duration: 0.5 }}
+            variants={{
+              visible: { opacity: 1, scale: 1 },
+              hidden: { opacity: 0, scale: 0 },
+            }} className={cl.contact_now_inner}>
             <div className={cl.contact_now_inner_left}>
               <Image src={icon} />
               <h3>Start Your Best it-Service for Business</h3>
@@ -48,7 +100,7 @@ const Form = () => {
             <div className={cl.contact_now_inner_right}>
               <button>Contact Now</button>
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
--- src/components/Ompanies/Ompanies.jsx
+++ src/components/Ompanies/Ompanies.jsx
@@ -4,13 +4,17 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css/pagination";
 import { Autoplay } from "swiper";
 import Image from "next/image";
-import q from "../../assets/swip1.jpg";
-import w from "../../assets/swip2.jpg";
-import e from "../../assets/swip3.jpg";
-import r from "../../assets/swip4.jpg";
+import q from '../../assets/projects/1.jpg'
+import w from '../../assets/projects/2.jpg'
+import e from '../../assets/projects/3.jpg'
+import r from '../../assets/projects/4.jpg'
+import t from '../../assets/projects/5.jpg'
+import y from '../../assets/projects/6.jpg'
+import u from '../../assets/projects/7.jpg'
+import i from '../../assets/projects/8.jpg'
+import o from '../../assets/projects/9.jpg'
 import { useMediaQuery } from "react-responsive";
-import style from "../Services/Services.module.scss";
-
+import { motion } from 'framer-motion';
 const Ompanies = () => {
   const laptop = useMediaQuery({ query: "(max-width: 1024px)" });
   const isPlanshet = useMediaQuery({ query: "(max-width: 768px)" });
@@ -18,8 +22,26 @@ const Ompanies = () => {
   return (
     <div className={cl.largest}>
       <div className={cl.largest_inner}>
-        <h2>Our Best projects</h2>
-        <div className={cl.header}>
+        <motion.h2
+          viewport={{ once: true }}
+          initial='hidden'
+          whileInView='visible'
+          transition={{ duration: 0.5 }}
+          variants={{
+            visible: { opacity: 1, scale: 1 },
+            hidden: { opacity: 0, scale: 0 },
+          }}
+        >Our Best projects</motion.h2>
+        <motion.div
+          viewport={{ once: true }}
+          initial='hidden'
+          whileInView='visible'
+          transition={{ duration: 0.5 }}
+          variants={{
+            visible: { opacity: 1, scale: 1 },
+            hidden: { opacity: 0, scale: 0 },
+          }}
+          className={cl.header}>
           <h2>
             We Business and{" "}
             <span className={"greenContent"}>It Solutions Of Total</span>
@@ -27,17 +49,24 @@ const Ompanies = () => {
           </h2>
           <span className={cl.typography}>
             <p>
-              Lorem ipsum is simply free text dolor sit amet, consectetur notted
-              adipisicing elit sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua lonm andhn.
+              Below you can see some of our best projects. Among them are websites, mobile applications for android and ios
             </p>
           </span>
-        </div>
-        <div className={cl.largest_inner_bottom}>
+        </motion.div>
+        <motion.div
+          viewport={{ once: true }}
+          initial='hidden'
+          whileInView='visible'
+          transition={{ duration: 0.5 }}
+          variants={{
+            visible: { opacity: 1, scale: 1 },
+            hidden: { opacity: 0, scale: 0 },
+          }}
+          className={cl.largest_inner_bottom}>
           <Swiper
             resizeObserver={true}
             rewind={true}
-            slidesPerView={laptop ? 2 : isPlanshet ? 2 : isMobile ? 1 : 4}
+            slidesPerView={(isPlanshet && 1) || (3 && isMobile && 2) || 4}
             spaceBetween={40}
             className={cl.mySwiper}
             autoplay={{
@@ -49,37 +78,61 @@ const Ompanies = () => {
             <SwiperSlide className={cl.swipItem}>
               <Image src={q} className={cl.img} />
               <div className={cl.card_text_block}>
-                <h3>Business and Marketing</h3>
+                <h3>Medcheck - mobile App(IOS/Android)</h3>
               </div>
             </SwiperSlide>
             <SwiperSlide className={cl.swipItem}>
               <Image src={w} className={cl.img} />
               <div className={cl.card_text_block}>
-                <h3>Product of Design</h3>
+                <h3>Navi Taxi - mobile App (IOS/Android)</h3>
               </div>
             </SwiperSlide>
             <SwiperSlide className={cl.swipItem}>
               <Image src={e} className={cl.img} />
 
               <div className={cl.card_text_block}>
-                <h3>Business and Expand</h3>
+                <h3>KVADRAT.KG - Web App</h3>
               </div>
             </SwiperSlide>
             <SwiperSlide className={cl.swipItem}>
               <Image src={r} className={cl.img} />
               <div className={cl.card_text_block}>
-                <h3>Marketind for Grown</h3>
+                <h3>SANRIJAPAN.COM - Web App</h3>
               </div>
             </SwiperSlide>
             <SwiperSlide className={cl.swipItem}>
-              <Image src={q} className={cl.img} />
+              <Image src={t} className={cl.img} />
               <div className={cl.card_text_block}>
-                <h3>Improve for Business</h3>
+                <h3>Agrimatco - mobile App (Android)</h3>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={cl.swipItem}>
+              <Image src={y} className={cl.img} />
+              <div className={cl.card_text_block}>
+                <h3>Mayram market - mobile App (IOS/Android)</h3>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={cl.swipItem}>
+              <Image src={u} className={cl.img} />
+              <div className={cl.card_text_block}>
+                <h3>G Shop- mobile App (IOS/Android)</h3>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={cl.swipItem}>
+              <Image src={i} className={cl.img} />
+              <div className={cl.card_text_block}>
+                <h3>T-EXPRESS.US - Web App</h3>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={cl.swipItem}>
+              <Image src={o} className={cl.img} />
+              <div className={cl.card_text_block}>
+                <h3>DLXLIMOUSINE.COM - Web App</h3>
               </div>
             </SwiperSlide>
           </Swiper>
           <button>View all Projects</button>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
